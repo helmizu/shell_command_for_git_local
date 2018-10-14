d31d0fb9224b69bfad730557a98967221ab76d3d
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2018-10-14 22:33:42 +0700

    kepon

--- changeGit.sh
+++ changeGit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git filter-branch --env-filter '
-OLD_EMAIL="ardhiyatma_wibawa_25rpl@student.smktelkom-mlg.sch.id"
+OLD_EMAIL="kevinmourel@gmail.com"
 CORRECT_NAME="Helmi Zulfikar"
 CORRECT_EMAIL="helmi.zulfikar20@gmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
